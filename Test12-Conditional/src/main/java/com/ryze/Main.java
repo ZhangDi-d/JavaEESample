92cf243fae8d9ff5ea407a2f410266f4c96a8915
ZhangDi-d <1772085788@qq.com>
2019-04-03 19:42:05 +0800

    20190403_03

---
+++ Test12-Conditional/src/main/java/com/ryze/Main.java
@@ -0,0 +1,15 @@
+package com.ryze;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xueLai on 2019/4/3.
+ */
+public class Main {
+    private Logger logger  = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args){
+
+    }
+}
