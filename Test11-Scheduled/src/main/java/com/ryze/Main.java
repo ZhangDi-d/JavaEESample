3eec3feb5716e9c168248bdd4d02e97733e26df0
ZhangDi-d <1772085788@qq.com>
2019-04-03 19:12:13 +0800

    20190403_01

--- Test11-Scheduled/src/main/java/com/ryze/Main.java
+++ Test11-Scheduled/src/main/java/com/ryze/Main.java
@@ -1,7 +1,28 @@
 package com.ryze;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by xueLai on 2019/3/28.
  */
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args){
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        ScheduledTaskService bean = context.getBean(ScheduledTaskService.class);
+        logger.info("当前时间:"+sdf.get().format(new Date()));
+    }
+
+    private final static ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<SimpleDateFormat>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("HH:mm:ss");
+        }
+    };
 }
