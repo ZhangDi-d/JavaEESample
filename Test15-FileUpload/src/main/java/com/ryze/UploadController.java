e2a34d71fde30c0b4b30bf71cb3c7135d33f2d28
ZhangDi-d <1772085788@qq.com>
2019-04-08 11:44:05 +0800

    20190408_02

--- Test15-FileUpload/src/main/java/com/ryze/UploadController.java
+++ Test15-FileUpload/src/main/java/com/ryze/UploadController.java
@@ -1,6 +1,7 @@
 package com.ryze;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,10 +14,10 @@ import java.io.IOException;
 /**
  * Created by xueLai on 2019/4/8.
  */
-@RestController
+@Controller
 public class UploadController {
     @ResponseBody
-    @RequestMapping(value="upload",method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+    @RequestMapping(value="/upload",method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
     public String upload(MultipartFile file) {
         try {
             FileUtils.writeByteArrayToFile(new File("C:\\Users\\张 迪\\Desktop\\workPro\\"+file.getOriginalFilename()),file.getBytes());
--- Test15-FileUpload/src/main/java/com/ryze/WebMVCStaticConfig.java
+++ Test15-FileUpload/src/main/java/com/ryze/WebMVCStaticConfig.java
@@ -3,7 +3,10 @@ package com.ryze;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -25,5 +28,16 @@ public class WebMVCStaticConfig extends WebMvcConfigurerAdapter {
         internalResourceViewResolver.setViewClass(JstlView.class);
         return internalResourceViewResolver;
 
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("/index");
+    }
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(1000000);
+        return resolver;
     }
 }
